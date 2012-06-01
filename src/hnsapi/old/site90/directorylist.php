8f127f7f7ba97ae08d5283fa13d03c890e382131
Andrew Gerst <gerst20051@gmail.com>
2012-06-01 02:37:30 -0400

    new images and whitespace fixes

--- src/hnsapi/old/site90/directorylist.php
+++ src/hnsapi/old/site90/directorylist.php
@@ -61,7 +61,7 @@ if ((substr($leadon, -1, 1) != '/') && $leadon != '') $leadon = $leadon . '/';
 $startdir = $leadon;
 
 if ($_GET['dir']) {
-	//check this is okay.
+	// check this is okay.
 	if (substr($_GET['dir'], -1, 1) != '/') {
 		$_GET['dir'] = $_GET['dir'] . '/';
 	}
@@ -93,7 +93,7 @@ if (!file_exists($opendir)) {
 clearstatcache();
 if ($handle = opendir($opendir)) {
 	while (false !== ($file = readdir($handle))) {
-		//first see if this file is required in the listing
+		// first see if this file is required in the listing
 		if ($file == "." || $file == "..") continue;
 		$discard = false;
 		for ($hi=0;$hi<sizeof($hide);$hi++) {
@@ -134,7 +134,7 @@ if ($handle = opendir($opendir)) {
 	closedir($handle); 
 }
 
-//sort our files
+// sort our files
 if ($_GET['sort'] == "date") {
 	@ksort($dirs, SORT_NUMERIC);
 	@ksort($files, SORT_NUMERIC);
@@ -160,55 +160,47 @@ $dirs = @array_values($dirs); $files = @array_values($files);
 </head>
 <body>
 <div id="container">
-  <div id="listingcontainer">
-    <div id="listingheader"> 
-	<div id="headerfile">File</div>
-	<div id="headersize">Size</div>
-	<div id="headermodified">Last Modified</div>
-	</div>
-    <div id="listing">
-	<?php
-	$class = 'b';
-	if ($dirok) {
-	?>
-	<div><a href="<?php echo $dotdotdir; ?>" class="<?php echo $class; ?>"><img src="http://www.000webhost.com/images/index/dirup.png" alt="Folder" /><strong>..</strong> <em>-</em> <?php echo date ("M d Y h:i:s A", filemtime($dotdotdir)); ?></a></div>
-	<?php
-		if ($class == 'b') $class = 'w';
-		else $class = 'b';
-	}
-	$arsize = sizeof($dirs);
-	for ($i=0;$i<$arsize;$i++) {
-	?>
-	<div><a href="<?php echo $leadon.$dirs[$i]; ?>" class="<?php echo $class; ?>"><img src="http://www.000webhost.com/images/index/folder.png" alt="<?php echo $dirs[$i]; ?>" /><strong><?php echo $dirs[$i]; ?></strong> <em>-</em> <?php echo date ("M d Y h:i:s A", filemtime($leadon.$dirs[$i])); ?></a></div>
-	<?php
-		if ($class == 'b') $class = 'w';
-		else $class = 'b';	
-	}
-	
-	$arsize = sizeof($files);
-	for ($i=0;$i<$arsize;$i++) {
-		$icon = 'unknown.png';
-		$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
-		$supportedimages = array('gif', 'png', 'jpeg', 'jpg');
-		$thumb = '';
-	
-		if ($filetypes[$ext]) {
-			$icon = $filetypes[$ext];
-		}
-		
-		$filename = $files[$i];
-		if (strlen($filename)>43) {
-			$filename = substr($files[$i], 0, 40) . '...';
-		}
-		
-		$fileurl = $leadon . $files[$i];
-	?>
-	<div><a href="<?php echo $fileurl; ?>" class="<?php echo $class; ?>"<?php echo $thumb2; ?>><img src="http://www.000webhost.com/images/index/<?php echo $icon; ?>" alt="<?php echo $files[$i]; ?>" /><strong><?php echo $filename; ?></strong> <em><?php echo round(filesize($leadon.$files[$i])/1024); ?>KB</em> <?php echo date("M d Y h:i:s A", filemtime($leadon.$files[$i])); ?><?php echo $thumb; ?></a></div>
-	<?php
-		if ($class == 'b') $class = 'w';
-		else $class = 'b';	
-	}	
-	?></div>
+<div id="listingcontainer">
+<div id="listingheader"> 
+<div id="headerfile">File</div>
+<div id="headersize">Size</div>
+<div id="headermodified">Last Modified</div>
+</div>
+<div id="listing">
+<?php
+$class = 'b';
+if ($dirok) {
+?>
+<div><a href="<?php echo $dotdotdir; ?>" class="<?php echo $class; ?>"><img src="dirup.png" alt="Folder" /><strong>..</strong> <em>-</em> <?php echo date ("M d Y h:i:s A", filemtime($dotdotdir)); ?></a></div>
+<?php
+	if ($class == 'b') $class = 'w';
+	else $class = 'b';
+}
+$arsize = sizeof($dirs);
+for ($i=0;$i<$arsize;$i++) {
+?>
+<div><a href="<?php echo $leadon.$dirs[$i]; ?>" class="<?php echo $class; ?>"><img src="folder.png" alt="<?php echo $dirs[$i]; ?>" /><strong><?php echo $dirs[$i]; ?></strong> <em>-</em> <?php echo date ("M d Y h:i:s A", filemtime($leadon.$dirs[$i])); ?></a></div>
+<?php
+	if ($class == 'b') $class = 'w';
+	else $class = 'b';	
+}
+$arsize = sizeof($files);
+for ($i=0;$i<$arsize;$i++) {
+	$icon = 'unknown.png';
+	$ext = strtolower(substr($files[$i], strrpos($files[$i], '.')+1));
+	$supportedimages = array('gif', 'png', 'jpeg', 'jpg');
+	$thumb = '';
+	if ($filetypes[$ext]) $icon = $filetypes[$ext];
+	$filename = $files[$i];
+	if (strlen($filename)>43) $filename = substr($files[$i], 0, 40) . '...';
+	$fileurl = $leadon . $files[$i];
+?>
+<div><a href="<?php echo $fileurl; ?>" class="<?php echo $class; ?>"<?php echo $thumb2; ?>><img src="http://www.000webhost.com/images/index/<?php echo $icon; ?>" alt="<?php echo $files[$i]; ?>" /><strong><?php echo $filename; ?></strong> <em><?php echo round(filesize($leadon.$files[$i])/1024); ?>KB</em> <?php echo date("M d Y h:i:s A", filemtime($leadon.$files[$i])); ?><?php echo $thumb; ?></a></div>
+<?php
+	if ($class == 'b') $class = 'w';
+	else $class = 'b';	
+}	
+?></div>
 </div>
 </div>
 </body>
